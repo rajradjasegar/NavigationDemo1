a37e94ec2a787bcc471589ae788d3e2c42249944
raj <radjasegar.raj@gmail.com>
2021-10-22 22:02:35 +0200

    second commit with files

---
+++ app/src/main/java/raj/training/navdemo1/SecondFragment.kt
@@ -0,0 +1,30 @@
+package raj.training.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import raj.training.navdemo1.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        val input = arguments?.getString("input_user")
+        binding.textView.text = input.toString()
+
+        return binding.root
+    }
+}
